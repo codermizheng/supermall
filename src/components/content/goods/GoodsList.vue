0b982f20797d03d87d5ae857449b379a3dd61100
mizheng <617489106@qq.com>
2021-09-11 19:04:28 +0800

    完成本周流行和商品(网络请求获取数据)

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="goods">
+      <goods-list-item v-for="(item,index) in goods" :key="index" :goodsItem='item'>{{goods}}</goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from 'components/content/goods/GoodsListItem'
+
+export default {
+  name:'GoodsList',
+  components:{
+   GoodsListItem,
+  },
+  props:{
+      goods:{
+          type:Array,
+          default(){
+              return [];
+          }
+      }
+  }
+}
+</script>
+
+<style>
+ .goods {
+   display: flex;
+   flex-wrap: wrap; /*不全挤在一行显示*/ 
+   justify-content: space-around; /*均等分*/
+   padding: 2px;
+ }
+
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,36 +5,140 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <home-feature-view></home-feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick='tabClick'></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+    <ul>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+      <li>hh</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar"
 import HomeSwiper from './childComps/HomeSwiper.vue'
 import HomeRecommendView from './childComps/HomeRecommendView.vue'
+import HomeFeatureView from './childComps/HomeFeatureView.vue'
 
-import {getHomeMultidata} from 'network/home';
+import NavBar from "components/common/navbar/NavBar"
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
+
+import {getHomeMultidata,getHomeGoods} from 'network/home';
 
 export default {
    name:'Home',
    components:{
      NavBar,
      HomeSwiper,
-     HomeRecommendView
+     HomeRecommendView,
+     HomeFeatureView,
+     TabControl,
+     GoodsList,
    },
    data() {
      return {
        banners:[],
-       recommends:[]
+       recommends:[],
+       goods:{
+         'pop':{page:0,list:[]},
+         'new':{page:0,list:[]},
+         'sell':{page:0,list:[]}
+       },
+       currentType : 'pop'
+     }
+   },
+   computed: {
+     showGoods(){
+       return this.goods[this.currentType].list
      }
    },
    created() {
      //1. 请求多个数据
-     getHomeMultidata().then(res=>{
-       console.log(res);
+     this.getHomeMultidata();
+     this.getHomeGoods('pop');
+     this.getHomeGoods('new')
+     this.getHomeGoods('sell')
+   },//created
+   methods: {
+     /*
+     * 事件监听相关方法
+     */
+    tabClick(index){
+      switch (index){
+        case 0:
+          this.currentType = 'pop';
+          break;
+        case 1:
+          this.currentType = 'new';
+          break;
+        case 2:
+          this.currentType = 'sell';
+          break;
+      }
+    },
+
+     /*
+     * 网络请求相关方法
+     */ 
+     getHomeMultidata(){
+        getHomeMultidata().then(res=>{
+      //  console.log(res);
        this.banners = res.data.data.banner.list;
        this.recommends = res.data.data.recommend.list;
+     });
+     },//getHomeMultidata
+      getHomeGoods(type){
+       const page = this.goods[type].page + 1;
+       getHomeGoods(type,page).then(res=>{
+      //  console.log(res);
+       this.goods[type].list.push(... res.data.data.list);
+       this.goods[type].page+=1;
      })
+      },//getHomeGoods
    },
 }
 </script>
@@ -44,5 +148,20 @@ export default {
   /* background-color: var(--color-tink); */
   background-color: #ff8198;
   color: white;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 999;
+}
+#home{
+  padding-top: 44px;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
 }
 </style>
